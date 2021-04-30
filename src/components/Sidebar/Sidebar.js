428d8157930f93097aefd900796a2ee394448fa2
paolo <paolo.morgano@smartlis.com>
2021-04-30 18:01:59 +0200

    feat(app): change logo

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -37,20 +37,12 @@ function Sidebar({ color, image, routes }) {
       />
       <div className="sidebar-wrapper">
         <div className="logo d-flex align-items-center justify-content-start">
-          <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
-            className="simple-text logo-mini mx-1"
-          >
-            <div className="logo-img">
-              <img
-                src={require("assets/img/reactlogo.png").default}
-                alt="..."
-              />
-            </div>
-          </a>
-          <a className="simple-text" href="http://www.creative-tim.com">
-            Creative Tim
-          </a>
+          <div className="logo-img">
+            <img
+              src={require("assets/img/sl-logo.png").default}
+              alt="..."
+            />
+          </div>
         </div>
         <Nav>
           {routes.map((prop, key) => {
